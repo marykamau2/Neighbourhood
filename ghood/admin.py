100c19e779c9764e721ea88d74f61f1ee1e4e898
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:33:28 +0300

    registered neighbourhood model

--- ghood/admin.py
+++ ghood/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Profile,Neighbourhood,Post,Business
 
 admin.site.register(Profile)
-# admin.site.register(Neighbourhood)
+admin.site.register(Neighbourhood)
 # admin.site.register(Post)
 # admin.site.register(Business)
