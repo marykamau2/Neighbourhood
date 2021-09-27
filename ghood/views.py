38fd56f7477995ddda5c9df81433e2006985855b
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:36:33 +0300

    create function view for the profile model

--- ghood/views.py
+++ ghood/views.py
@@ -1,31 +1,31 @@
-# from django.shortcuts import redirect,render, get_object_or_404
-# from django.http import HttpResponseRedirect
-# from django.contrib.auth import login, authenticate
-# from django.contrib.auth.models import User
-# from .forms import SignUpForm, BusinessForm,UpdateProfileForm, NeighbourHoodForm, PostForm
-# from django.contrib.auth.decorators import login_required
-# from .models import Neighbourhood, Profile, Business, Post 
+from django.shortcuts import redirect,render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from .forms import SignUpForm, BusinessForm,UpdateProfileForm, NeighbourHoodForm, PostForm
+from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood, Profile, Business, Post 
 
 
 
-# @login_required(login_url='login')
-# def index(request):
-#     return render(request, 'index.html')
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
 
 
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-#             return redirect('index')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'registration/signup.html', {'form': form})
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
     
 
 # def edit_profile(request, username):
