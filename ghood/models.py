966f0061c043cb51f0a614b27a3656a26a7e9845
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:24:22 +0300

    added the profile model

--- ghood/models.py
+++ ghood/models.py
@@ -1,28 +1,28 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.dispatch import receiver
-# from django.db.models.signals import post_save
-# import datetime as dt
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import datetime as dt
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-#     name = models.CharField(max_length=80, blank=True)
-#     bio = models.TextField(max_length=254, blank=True)
-#     profile_picture = models.ImageField(upload_to='images/', default='default.png')
-#     location = models.CharField(max_length=50, blank=True, null=True)
-#     neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
-#     def __str__(self):
-#         return f'{self.user.username} profile'
+    def __str__(self):
+        return f'{self.user.username} profile'
 
-#     @receiver(post_save, sender=User)
-#     def create_user_profile(sender, instance, created, **kwargs):
-#         if created:
-#             Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-#     @receiver(post_save, sender=User)
-#     def save_user_profile(sender, instance, **kwargs):
-#         instance.profile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 # class Neighbourhood(models.Model):
