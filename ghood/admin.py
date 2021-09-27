1db671e4d0372c2690fb8c106ee558092451381c
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:15:06 +0300

    added templates

--- ghood/admin.py
+++ ghood/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Profile,Neighbourhood,Post,Business
 
-# Register your models here.
+# admin.site.register(Profile)
+# admin.site.register(Neighbourhood)
+# admin.site.register(Post)
+# admin.site.register(Business)
