90b60edbbdebb6fdbecc4921e05b638a2f933776
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:43:03 +0300

    registered the post model

--- ghood/admin.py
+++ ghood/admin.py
@@ -3,5 +3,5 @@ from .models import Profile,Neighbourhood,Post,Business
 
 admin.site.register(Profile)
 admin.site.register(Neighbourhood)
-# admin.site.register(Post)
+admin.site.register(Post)
 # admin.site.register(Business)
