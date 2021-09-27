e8f49059889188226adf0d7c30df753a6ae7c701
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:19:54 +0300

    create url to store the urlpatterns

--- ghood/urls.py
+++ ghood/urls.py
@@ -1,13 +1,13 @@
-# from django.urls import path, include
-# from . import views
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('register/', views.signup, name='signup'),
-#     path('account/', include('django.contrib.auth.urls')),
-#     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
 #     path('profile/<username>', views.profile, name='profile'),
 #     path('all-hoods/',views.neighbourhoods,name='hood'),
 #     path('new-hood/', views.create_neighbourhood, name='new-hood'),
