aaf84c20d1d99a511cfad8575b1fbe82cc53c937
marykamau2 <mary.kamau@student.moringaschool.com>
2021-09-27 10:24:59 +0300

    registered the profile model

--- ghood/admin.py
+++ ghood/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import Profile,Neighbourhood,Post,Business
+from django.contrib import admin
+from .models import Profile,Neighbourhood,Post,Business
 
-# admin.site.register(Profile)
+admin.site.register(Profile)
 # admin.site.register(Neighbourhood)
 # admin.site.register(Post)
 # admin.site.register(Business)
